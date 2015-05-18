06e03417e2da9332542e39a7a590cc2a0d29b294
naguraghu99 <nagu.raghu99@gmaial.com>
2015-05-18 20:59:32 +0530

    to demonstrate copy constructor and assignent operator overload

---
+++ c++/copyctor.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+ 
+class Test
+{
+    int *ptr;
+public:
+    Test (int i = 0)      { ptr = new int(i); }
+    void setValue (int i) { *ptr = i; }
+    void print()          { cout << *ptr << endl; }
+    
+     //copy constructor
+     Test(const Test &t){
+	     cout<<"Copy constructor is invoked "<<endl;
+	     if(this != &t)
+		     *ptr = *(t.ptr);
+     }
+    //asignment operator overload 
+    Test & operator = (const Test &t);
+};
+ 
+Test & Test::operator = (const Test &t)
+{
+   // Check for self assignment
+   if(this != &t)
+     *ptr = *(t.ptr);
+ 
+   return *this;
+}
+ 
+int main()
+{
+    Test t1(5);
+    Test t2;
+    t2 = t1;
+    t1.setValue(10);
+    Test t3(t1);
+    t2.print();
+    t3.print();
+    return 0;
+}
